0f3dbbfb7fcabe8ba9f4266aa5ccfa2994ae5d4c
alstjd0051 <wsc03002@naver.com>
2022-10-06 02:37:46 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div className="w-full h-full">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
