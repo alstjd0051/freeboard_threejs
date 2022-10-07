a6246c230078d876a67021937fe3d6edaa4ea837
alstjd0051 <wsc03002@naver.com>
2022-10-07 17:35:03 +0900

    update:: styled

--- components/commons/unit/laytout/nav/Navbar.tsx
+++ components/commons/unit/laytout/nav/Navbar.tsx
@@ -9,13 +9,15 @@ const Navbar = ({ children }: Props) => {
   return (
     <div className="flex border-8 border-solid border-black ">
       <div className="w-full h-screen">{children}</div>
-      <nav className="hidden md:flex  flex-col text-center items-center justify-center md:border-8 w-1/4   border-solid md:border-blue-500 h-screen  ">
-        <Link href={"/BoxPage"}>
-          <a className="text-lg  font-bold">Move To BoxPage</a>
-        </Link>
-        <Link href={"/BirdPage"}>
-          <a className="text-lg font-bold">Move To BirdPage</a>
-        </Link>
+      <nav className="hidden md:flex  sm:w-full  flex-col text-center items-center justify-center md:border-8 md:w-1/4   border-solid md:border-blue-500 h-screen  ">
+        <div className="flex-col flex py-10">
+          <Link href={"/BoxPage"}>
+            <a className="text-lg  font-bold">Move To BoxPage</a>
+          </Link>
+          <Link href={"/BirdPage"}>
+            <a className="text-lg font-bold">Move To BirdPage</a>
+          </Link>
+        </div>
       </nav>
     </div>
   );
