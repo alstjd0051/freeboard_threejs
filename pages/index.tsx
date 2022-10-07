23b4b53a5a58c268cd34ce5807674c03ccaed9d4
alstjd0051 <wsc03002@naver.com>
2022-10-07 17:17:01 +0900

    Create Project(default value)

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Home;
