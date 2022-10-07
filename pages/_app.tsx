648799d6d615735abcad3bf23fd9fee2c27bbe0f
alstjd0051 <wsc03002@naver.com>
2022-10-07 17:21:44 +0900

    Update:: Change folder name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Navbar from "../components/commons/unit/laytou/nav/Navbar";
+import Navbar from "../components/commons/unit/laytout/nav/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
