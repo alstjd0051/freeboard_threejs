457c11eb9094e9155e42ba3aba9bbf87dda7e4d5
alstjd0051 <wsc03002@naver.com>
2022-10-11 21:53:15 +0900

    Create:: ScrollModelAnimation

--- components/commons/three/model/ScrollModelAnimation.tsx
+++ components/commons/three/model/ScrollModelAnimation.tsx
@@ -1,18 +1,18 @@
-import { count } from "console";
 import React, { useEffect, useState, useRef } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import gsap from "gsap";
+import { loadGLTFModel } from "../../../../lib/model";
 import { DivContainer, DivSpinner } from "../../../unit/ScrollPage";
 
-const ModelAnimation = () => {
-  const [loading, setLoading] = useState<boolean>(true);
+const ScrollModelAnimation = () => {
+  const [loading, setLoading] = useState(true);
   const [renderer, setRenderer] = useState();
-  const [_camera, set_camera] = useState();
+  const [_camera, setCamera] = useState();
   const [scene] = useState(new THREE.Scene());
   const [_controls, setControls] = useState();
 
-  const refContainer = useRef<any>();
+  const refContainer = useRef();
 
   useEffect(() => {
     const parameters = {
@@ -21,28 +21,62 @@ const ModelAnimation = () => {
     const { current: container } = refContainer;
 
     if (container && !renderer) {
-      // Texture
+      //Texture
       const textureLoader = new THREE.TextureLoader();
-      const gradientTexture = textureLoader.load("texture/gradients/3.jpg");
-      const particlesTexture = textureLoader.load("texture/particles/1.png");
+      const eartTexture = textureLoader.load("/textures/earth/earth.jpeg");
+      const moonTexture = textureLoader.load("/textures/moon/moon.jpeg");
+      const sunTexture = textureLoader.load("/textures/sun/sun.jpeg");
+      const gradientTexture = textureLoader.load("/textures/gradients/3.jpg");
+      gradientTexture.magFilter = THREE.NearestFilter;
+      const particlesTexture = textureLoader.load("/textures/particles/1.png");
 
-      // Material
+      //Material
       const material = new THREE.MeshToonMaterial({
         color: parameters.materialColor,
         gradientMap: gradientTexture,
       });
 
-      // Object
+      //Object
+
+      loadGLTFModel(scene, "/Desktop.glb", {
+        receiveShadow: true,
+        castShadow: true,
+      }).then(() => {
+        setLoading(false);
+      });
+
       const objectDistance = 4;
       const mesh1 = new THREE.Mesh(
         new THREE.TorusGeometry(1, 0.4, 16, 60),
         material
       );
-      const mesh2 = new THREE.Mesh(new THREE.SphereGeometry(1), material);
 
-      const mesh3 = new THREE.Mesh(new THREE.SphereGeometry(1), material);
-      const mesh4 = new THREE.Mesh(new THREE.SphereGeometry(1), material);
-      const mesh5 = new THREE.Mesh(new THREE.SphereGeometry(1), material);
+      const mesh2 = new THREE.Mesh(
+        new THREE.SphereGeometry(1),
+        new THREE.MeshStandardMaterial({
+          map: moonTexture,
+        })
+      );
+
+      const mesh3 = new THREE.Mesh(
+        new THREE.SphereGeometry(1),
+        new THREE.MeshStandardMaterial({
+          map: eartTexture,
+        })
+      );
+      const mesh4 = new THREE.Mesh(
+        new THREE.SphereGeometry(1),
+        new THREE.MeshStandardMaterial({
+          map: sunTexture,
+        })
+      );
+      const mesh5 = new THREE.Mesh(
+        new THREE.SphereGeometry(1),
+        new THREE.MeshStandardMaterial({
+          color: "red",
+          wireframe: true,
+        })
+      );
 
       mesh1.position.x = 3;
       mesh2.position.x = -3;
@@ -58,39 +92,39 @@ const ModelAnimation = () => {
 
       scene.add(mesh1, mesh2, mesh3, mesh4, mesh5);
       const sectionMeshes = [mesh1, mesh2, mesh3, mesh4, mesh5];
+      mesh1.visible = false;
 
-      //   Lights
-      const directionLight = new THREE.DirectionalLight(0xffffffff, 1);
-      directionLight.position.set(1, 1, 0);
-      directionLight.castShadow = true;
-      scene.add(directionLight);
+      //Lights
+      const directioanlLight = new THREE.DirectionalLight(0xffffff, 1);
+      directioanlLight.position.set(1, 1, 0);
+      directioanlLight.castShadow = true;
+      scene.add(directioanlLight);
 
-      const ambientLight = new THREE.AmbientLight(0xffffffff, 0.5);
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
       scene.add(ambientLight);
 
-      // Particle geometry
+      //Particle geometry
       const particlesGeometry = new THREE.BufferGeometry();
       const count = 1000;
-      const position = new Float32Array(count * 3);
+      const positions = new Float32Array(count * 3);
       const colors = new Float32Array(count * 3);
-      for (let i = 0; i < count; i++) {
-        position[i * 3 + 0] = (Math.random() - 0.5) * 10;
-        position[i * 3 + 1] =
+      for (let i = 0; i < count * 3; i++) {
+        positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
+        positions[i * 3 + 1] =
           objectDistance * 0.5 -
           Math.random() * objectDistance * sectionMeshes.length;
-        objectDistance * sectionMeshes.length;
-        position[i * 3 + 2] = (Math.random() - 0.5) * 10;
+        positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
         colors[i] = Math.random();
       }
       particlesGeometry.setAttribute(
         "position",
-        new THREE.BufferAttribute(position, 3)
+        new THREE.BufferAttribute(positions, 3)
       );
       particlesGeometry.setAttribute(
         "color",
         new THREE.BufferAttribute(colors, 3)
       );
-      // Particles Material
+      //Particles Material
       const particlesMaterial = new THREE.PointsMaterial();
       particlesMaterial.size = 0.1;
       particlesMaterial.sizeAttenuation = true;
@@ -98,7 +132,7 @@ const ModelAnimation = () => {
       particlesMaterial.transparent = true;
       particlesMaterial.alphaMap = particlesTexture;
       particlesMaterial.depthWrite = false;
-      particlesMaterial.depthTest = false;
+      //particlesMaterial.depthTest = false;
       particlesMaterial.blending = THREE.AdditiveBlending;
       particlesMaterial.vertexColors = true;
 
@@ -106,18 +140,17 @@ const ModelAnimation = () => {
       const particles = new THREE.Points(particlesGeometry, particlesMaterial);
       scene.add(particles);
 
-      // size
+      //sizes
       const sizes = {
         width: window.innerWidth,
         height: window.innerHeight,
       };
-
-      // camera
-      // Camera Group
+      //camera
+      //Camera Group
       const cameraGroup = new THREE.Group();
       scene.add(cameraGroup);
 
-      // Base Camera
+      //Base Camera
       const camera = new THREE.PerspectiveCamera(
         35,
         sizes.width / sizes.height,
@@ -128,7 +161,7 @@ const ModelAnimation = () => {
 
       cameraGroup.add(camera);
 
-      // Renderer
+      //Renderer
       const renderer = new THREE.WebGLRenderer({
         antialias: true,
         alpha: true,
@@ -139,13 +172,13 @@ const ModelAnimation = () => {
       renderer.shadowMap.enabled = true;
       container.appendChild(renderer.domElement);
 
-      // Resize
+      //Resize
       window.addEventListener("resize", () => {
-        // update sizes
+        //update sizes
         sizes.width = window.innerWidth;
         sizes.height = window.innerHeight;
 
-        // update camera
+        //update camera
         camera.aspect = sizes.width / sizes.height;
         camera.updateProjectionMatrix();
 
@@ -157,7 +190,7 @@ const ModelAnimation = () => {
       const controls = new OrbitControls(camera, renderer.domElement);
       controls.autoRotate = true;
 
-      // Scroll
+      //Scroll
       let scrollY = window.scrollY;
       let currentSection = 0;
 
@@ -165,7 +198,7 @@ const ModelAnimation = () => {
         scrollY = window.scrollY;
         const newSection = Math.round(scrollY / sizes.height);
 
-        if (newSection !== currentSection) {
+        if (newSection != currentSection) {
           currentSection = newSection;
 
           gsap.to(sectionMeshes[currentSection].rotation, {
@@ -177,7 +210,8 @@ const ModelAnimation = () => {
           });
         }
       });
-      // Cursor
+
+      //Cursor
       const cursor = {};
       cursor.x = 0;
       cursor.y = 0;
@@ -186,35 +220,36 @@ const ModelAnimation = () => {
         cursor.x = e.clientX / sizes.width - 0.5;
         cursor.y = e.clientY / sizes.height - 0.5;
       });
-      // Animate
+      //Animate
       const clock = new THREE.Clock();
       let previousTime = 0;
 
       const animate = () => {
-        const colapsedTime = clock.getElapsedTime();
-        const deltaTime = colapsedTime - previousTime;
-        previousTime = deltaTime;
+        const elapsedTime = clock.getElapsedTime();
+        const deltaTime = elapsedTime - previousTime;
+        previousTime = elapsedTime;
 
-        // animate particles
-        particles.rotation.y = colapsedTime * 2;
+        //animate particles
+        particles.rotation.y = elapsedTime * 0.02;
 
-        // animate camera
+        //animate camera
         camera.position.y = (-scrollY / sizes.height) * objectDistance;
 
         const parallaxX = -cursor.x * 0.5;
-        const parallaxY = -cursor.y * 0.5;
+        const parallaxY = cursor.y * 0.5;
 
         cameraGroup.position.x +=
           (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
         cameraGroup.position.y +=
           (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
 
-        // animate meshes
+        //animate meshes
         for (const mesh of sectionMeshes) {
-          mesh.rotation.x += 0.1 * deltaTime;
-          mesh.rotation.y += 0.12 * deltaTime;
+          mesh.rotation.x += deltaTime * 0.1;
+          mesh.rotation.y += deltaTime * 0.12;
         }
 
+        //render
         renderer.render(scene, camera);
         window.requestAnimationFrame(animate);
       };
@@ -227,4 +262,4 @@ const ModelAnimation = () => {
   );
 };
 
-export default ModelAnimation;
+export default ScrollModelAnimation;
--- components/unit/ScrollPage.tsx
+++ components/unit/ScrollPage.tsx
@@ -4,7 +4,7 @@ export const DivSpinner = () => <div>Loading...</div>;
 
 export const DivContainer = forwardRef(({ children }, ref) => (
   <>
-    <div ref={ref} className="fixed top-0 left-0 outline-none "></div>
+    <div ref={ref} className="fixed top-0 left-0 outline-none"></div>
     <section className="flex items-center h-[100vh] relative text-lg uppercase pl-[10%] pr-[10%] pt-[100px] font-neue text-[#ffeded] ">
       Section1
     </section>
