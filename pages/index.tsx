299e4efc0590892f74528f6d4f6d22a22a4bf0dd
alstjd0051 <wsc03002@naver.com>
2022-10-11 19:55:16 +0900

    Update:: HomeMessage

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,12 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <div></div>
+    <div className="w-full h-full flex items-center justify-center  ">
+      <div className="snap-center">
+        <h1 className="font-bold md:text-7xl mi:text-3xl ">
+          Welcome to tyler board
+        </h1>
+      </div>
     </div>
   );
 };
